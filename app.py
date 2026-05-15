47f35c5ae466eee28088f4c14d876688aa26279c
fathimaabduljabbar8590-ops <fathimaabduljabbar8590@gmail.com>
2026-05-15 22:14:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def home():
 
         data = response.json()
 
-        if "matches" in data:
+         if "matches" in data or "unsafe" in url or "phishing" in url:
             result = "⚠️ Unsafe Website Detected!"
         else:
             result = "✅ This URL Looks Safe"
